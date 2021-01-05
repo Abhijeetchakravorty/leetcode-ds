124b08cc6438d9cfca1caab6f4ccb31f30530fdc
Abhijeet Chakravorty <>
2021-01-05 12:57:37 +0530

    updated code for checking double

--- leetcode/arrays/searchingArray/checkDouble.py
+++ leetcode/arrays/searchingArray/checkDouble.py
@@ -11,19 +11,21 @@ class Solution:
                         while(self.__j__ < self.data):
                                 self.num = int(input("Please provide a number: "))
                                 self.arr.append(self.num)
+                                self.__j__ += 1
                 else:
-                        print("Please provide the length")
+                        print("Value out of range")
                         self.add_items()
 
         def checkDouble(self):
                 self.found = False
-                for __i in range(len(self.data)):
-                        for __j in range(len(self.data)):
-                                if ((__i != __j and self.data[__i] != self.data[__j]) and (self.data[__i] * 2 == self.data[__j]) or (self.data[__j] == self.data[__i] * 2)):
+                for __i in range(self.data):
+                        for __j in range(self.data):
+                                if ((__i != __j) and (self.arr[__i] != self.arr[__j]) and ((self.arr[__i] == self.arr[__j] * 2) or (self.arr[__i]/2 == float(self.arr[__j])) or (float(self.arr[__i]) == self.arr[__j]/2))):
                                         self.found = True
                                         break
-                        break
+                        if(self.found):
+                                break
                 return self.found
 
 p1 = Solution()
-p1.checkDouble()
+print(p1.checkDouble())
