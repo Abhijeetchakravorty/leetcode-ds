1dc314c41fa323e451346b1f5ef7767fb9b242a7
Abhijeet Chakravorty <>
2021-01-06 18:02:07 +0530

    updated code for srted squared array

--- leetcode/arrays/conclusions/squareSortedArr.py
+++ leetcode/arrays/conclusions/squareSortedArr.py
@@ -7,9 +7,15 @@ class Solution:
         def addToList(self, indx):
                 if (indx < self.data):
                         self.num = int(input("PLease provide a height: "))
-                        if (self.num < 1):
-                                print("The minimum number is 1")        
-                        else:
-                                self.arr.append(self.num)
-                                indx += 1
+                        self.arr.append(self.num)
+                        indx += 1
                         self.addToList(indx)
+        
+        def squaredSortedArr(self):
+                for i in range(self.data):
+                        self.arr[i] = pow(self.arr[i], 2)
+                self.arr = sorted(self.arr)
+                return self.arr
+
+p1 = Solution()
+print(p1.squaredSortedArr())
