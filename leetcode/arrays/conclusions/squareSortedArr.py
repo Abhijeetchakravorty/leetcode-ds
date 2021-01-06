8a264792d3fdff178512798b671d4f14a5338a46
Abhijeet Chakravorty <>
2021-01-06 17:14:40 +0530

    updated code for master

---
+++ leetcode/arrays/conclusions/squareSortedArr.py
@@ -0,0 +1,15 @@
+class Solution:
+        def __init__(self):
+                self.arr = []
+                self.data = int(input("Please provide the length of the array: "))
+                self.addToList(0)
+        
+        def addToList(self, indx):
+                if (indx < self.data):
+                        self.num = int(input("PLease provide a height: "))
+                        if (self.num < 1):
+                                print("The minimum number is 1")        
+                        else:
+                                self.arr.append(self.num)
+                                indx += 1
+                        self.addToList(indx)
