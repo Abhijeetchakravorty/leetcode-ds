422b921553dd3d94024b7572b1216573a51b337c
Abhijeet Chkravorty <chakravorty.abhijeet91@gmail.com>
2021-05-25 15:23:51 +0530

    updated code

---
+++ leetcode/Javascript/linkedlist/design.js
@@ -0,0 +1,79 @@
+class Node {
+    constructor(elem) {
+        this.element = elem;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class MyLinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    getVal(index) {
+        let counter = 0;
+        let temp = this.head;
+        while(temp) {
+            if (temp.next !== null && temp.next !== undefined) {
+                if (counter == index) {
+                    break;
+                } else {
+                    temp = temp.next;
+                }
+            }
+        }
+        return temp;
+    }
+
+    addAtHead(start) {
+        start.next = this.head;
+		this.head = start;
+    }
+
+    addAtTail(){
+        let temp = this.head;
+		let last = false;
+		while(temp) {
+			if (temp.next == null) {
+				temp.next = endingNode;
+				break;
+			} else {
+				temp = temp.next;
+			}
+		}
+    }
+
+    addAtIndex(pos, node) {
+        let counter = 1;
+        let temporary = this.head;
+        while(counter < pos) {
+            if (counter+1==pos) {
+                let tempNext = temporary.next;
+                let tempPrev = temporary.next.prev;
+                console.log(tempNext.elem);
+                console.log(tempPrev.elem);
+                node.next = tempNext;
+                node.prev = tempPrev;
+                temporary.next = node;
+                counter++;
+            } else {
+                temporary = temporary.next;
+                counter++;
+            }
+
+            if (temporary.next == null || temporary.next == undefined) {
+                break;
+            }
+        }
+    }
+
+    deleteAtIndex() {
+        
+    }
+
+    addNeighbour(src, dest) {
+		src.next = dest;
+		dest.prev = src;
+	}
+}
