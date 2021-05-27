28535cff743d5f0509c3981c0711b36ac0c14b1b
Abhijeet Chkravorty <chakravorty.abhijeet91@gmail.com>
2021-05-27 12:18:26 +0530

    updated code

--- leetcode/Javascript/linkedlist/randompointer.js
+++ leetcode/Javascript/linkedlist/randompointer.js
@@ -1,7 +1,7 @@
 //// a linked list length n
 //// Construct a deep copy of the list
 //// The deep copy should consist of exactly n brand new nodes, where each new node has its value set to the value of its corresponding original node.
-////
+//// 72 - 96 ms
 
 class Node {
         constructor(elem) {
@@ -47,7 +47,6 @@ class LinkedList {
 		// and link them together side-by-side in a single list.
 		while (iter != null) {
 			next = iter.next;
-
 			let copy = new RandomListNode(iter.val);
 			iter.next = copy;
 			copy.next = next;
@@ -62,6 +61,7 @@ class LinkedList {
 			}
 			iter = iter.next.next;
 		}
+
 		// Third round: restore the original list, and extract the copy list.
 		iter = head;
 		let pseudoHead = new RandomListNode(0);
@@ -104,7 +104,6 @@ llist.addRandom(third, second);
 llist.addRandom(fourth, llist.head);
 
 llist.printList(llist.head);
-
 
 console.log('///////////////////////');
 console.log('///////////////////////');
